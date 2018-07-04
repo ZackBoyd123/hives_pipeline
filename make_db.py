0cf8d947d06780ce6645d5571008ac258d242640
Zack Boyd <zack.boyd@glasgow.ac.uk>
2018-07-04 12:45:20 +0100

    changed the make db script to accomodate the change of outfmt 6 in blast pipeline

--- make_db.py
+++ make_db.py
@@ -1,26 +1,27 @@
-#!/usr/local/bin/python3 
+#!/usr/local/bin/python3
 import csv
 import argparse
 
 parser = argparse.ArgumentParser(description= "A script which melts the blast outfmt 6 format into a " +
-                    "format ready to be loaded into SQL. This was written specifically " + 
-                    "for blasting sequences against viral sequnces, so the headers are tailored " + 
+                    "format ready to be loaded into SQL. This was written specifically " +
+                    "for blasting sequences against viral sequnces, so the headers are tailored " +
                     "toward that")
 parser.add_argument("--input", required=True, help="Input file: this must be produced from blast with the" +
                     "--outfmt 6 option specified.")
 parser.add_argument("--output", required=True, help="What you want your output file to be called. ")
+parser.add_argument("--retro", required=True, help="A file containing one column of all retrovirus taxids.")
 args = parser.parse_args()
 
-
-#'''
-# To do add in whether or not query is a retro virus.
-#'''
 out_file = open(args.output,"w")
 
-print("Query\t"+"Subject\t"+"Percentage\t"+"Alignment_length\t"+"Mismatches\t"+"Gap_openings\t"+
-      "Query_start\t"+"Query_end\t"+"Subject_start\t"+"Subject_end\t"+"Evalue\t"+"Bitscore\t"+
-      "Query_accession\t"+"Query_taxid\t"+"Query_html\t"+"Subject_accession\t"+"Subject_taxid\t"+
-      "Subject_html",file=out_file)
+print("Query\t"+"Subject\t"+"Percentage\t"+"Alignment_length\t"+"Query_start\t"+"Query_end\t"+"Query_len\t"
+      +"Subject_start\t"+"Subject_end\t"+"Subject_len\t"+"Evalue\t"+"Bitscore\t"+"Retrovirus\t"+
+      "Query_accession\t"+"Query_taxid\t"+"Query_div\t"+"Query_html\t"+"Subject_accession\t"+"Subject_taxid\t"+
+      "Subject_div\t"+"Subject_html",file=out_file)
+
+retro_file = open(args.retro)
+retro_list = retro_file.readlines()
+retro_list = [i.replace("\n","") for i in retro_list]
 
 html_stub = "https://www.ncbi.nlm.nih.gov/nuccore/"
 with open(args.input) as f:
@@ -29,9 +30,16 @@ with open(args.input) as f:
 
         query_acc = "".join(line[0].split("|")[3])
         query_taxid = "".join(line[0].rsplit("|")[-3])
+        query_div = "".join(line[0].rsplit("|")[-2])
         subject_acc = "".join(line[1].split("|")[3])
         subject_taxid = "".join(line[1].rsplit("|")[-3])
+        subject_div = "".join(line[1].rsplit("|")[-2])
 
-        print("\t".join(line)+"\t"+query_acc+
-              "\t"+query_taxid+"\t"+html_stub+query_acc+"\t"+
-              subject_acc+"\t"+subject_taxid+"\t"+html_stub+subject_acc,file=out_file)
+        if query_taxid in retro_list:
+            is_retro = "YES"
+        else:
+            is_retro = "NO"
+
+        print("\t".join(line)+"\t"+is_retro+"\t"+query_acc+
+              "\t"+query_taxid+"\t"+query_div+"\t"+html_stub+query_acc+"\t"+
+              subject_acc+"\t"+subject_taxid+"\t"+subject_div+"\t"+html_stub+subject_acc,file=out_file)
